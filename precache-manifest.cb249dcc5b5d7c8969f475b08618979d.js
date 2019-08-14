f5a442d9d697f03e79b86d3feb20d992bed28dbf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-14 05:40:06 +0000

    Deploy JeremyCraigMartinez/tlsalon to github.com/JeremyCraigMartinez/tlsalon.git:gh-pages

---
+++ precache-manifest.cb249dcc5b5d7c8969f475b08618979d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3698bff8178b9ce8e2b165a4a34bde3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c513735731e84eaf41",
+    "url": "/static/js/2.ec3e83da.chunk.js"
+  },
+  {
+    "revision": "2b2d6afdbc4882b7e3bf",
+    "url": "/static/js/main.ecf8aa91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efdaed276f77abd83028d36a46b10aad.js"
+  "/precache-manifest.cb249dcc5b5d7c8969f475b08618979d.js"
 );
 
 self.addEventListener('message', (event) => {
